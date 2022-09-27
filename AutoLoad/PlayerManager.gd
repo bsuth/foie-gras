8fc51d58466389b3f6345e81c44b9d853e0eda62
bsuth <bsuth701@gmail.com>
2022-09-27 23:04:23 +0900

    update

--- AutoLoad/PlayerManager.gd
+++ AutoLoad/PlayerManager.gd
@@ -3,28 +3,33 @@ extends Node
 signal player_connected(Player)
 signal player_disconnected(Player)
 
-var players: Array = []
+var players: Array = [
+	Player.new(),
+	Player.new(),
+	Player.new(),
+	Player.new(),
+]
 
 class Player:
-	var device: int
-	var connected: bool
+	var device: int = -1
 
 func _ready():
+	var connected_joypads = Input.get_connected_joypads()
+	for i in range(min(players.size(), connected_joypads.size())):
+		players[i].device = connected_joypads[i]
+		emit_signal("player_connected", players[i])
 	Input.connect("joy_connection_changed", self, "on_joy_connection_changed")
 
 func on_joy_connection_changed(device, connected):
-	var existing_player: Player
-	for player in players:
-		if player.device == device:
-			existing_player = player
-			break
-	
-	if not existing_player:
-		existing_player = Player.new()
-		existing_player.device = device
-
-	existing_player.connected = connected
 	if connected:
-		emit_signal("player_connected", existing_player)
+		for player in players:
+			if player.device == -1:
+				player.device = device
+				emit_signal("player_connected", player)
+				break
 	else:
-		emit_signal("player_disconnected", existing_player)
+		for player in players:
+			if player.device == device:
+				player.device = -1
+				emit_signal("player_disconnected", player)
+				break
